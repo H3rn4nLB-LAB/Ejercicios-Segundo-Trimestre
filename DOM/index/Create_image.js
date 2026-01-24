598a78d09c50eaae91ab1ef7cea5c2c67618f32e
H3rn4nLB-LAB <hernanlb06@gmail.com>
2026-01-24 12:18:57 +0100

    Update Create_image.js

--- DOM/index/Create_image.js
+++ DOM/index/Create_image.js
@@ -2,7 +2,7 @@ function agregarImagen() {
 
   var img = document.createElement("img");
 
-  img.src = "https://via.placeholder.com/250";   
+  img.src = "https://picz.dev/landscape";   
   img.alt = "Imagen generada por JavaScript";
   img.style.width = "100%"; 
 
