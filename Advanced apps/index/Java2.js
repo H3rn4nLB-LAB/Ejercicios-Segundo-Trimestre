cd4e8c17bd8c5f52e4dbf32dd2aa382b47c75974
H3rn4nLB-LAB <hernanlb06@gmail.com>
2026-01-30 09:51:40 +0100

    Create Java2.js

---
+++ Advanced apps/index/Java2.js
@@ -0,0 +1,11 @@
+function changeFontSize(size) {
+    document.getElementById("content").style.fontSize = size + "px";
+}
+
+function changeTextColor(color) {
+    document.getElementById("content").style.color = color;
+}
+
+function changeBackgroundColor(color) {
+    document.getElementById("content").style.backgroundColor = color;
+}
